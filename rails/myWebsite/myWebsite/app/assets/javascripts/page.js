3d97e1ccd0b9c839c9d17e3e1bfc1b590fd141b6
dabbu94 <leo.giroux@epitech.eu>
2015-06-19 16:43:24 +0200

    all

--- rails/myWebsite/myWebsite/app/assets/javascripts/page.js
+++ rails/myWebsite/myWebsite/app/assets/javascripts/page.js
@@ -1,6 +1,18 @@
-$(function(){
 
-$('#first-row').hide();
-$('#first-row').fadeIn(1000);
+$(document).ready(function(){
 
+  $('.title').hide();
+  $('.title').fadeIn(1500);
+  $("#t_whoami").hide();
+  $('.scrollspy').scrollSpy();
+  
+  $(document).scroll(function () {
+    if ($("#whoami").scrollTop() == 0) {
+        $('#t_whoami').fadeIn(1500);
+    }
 });
+
+
+ 
+});
+
